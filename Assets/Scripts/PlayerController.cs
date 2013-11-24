ec56e67c54436a73bf8636626120163147a4ad21
Miguel Catalan Bañuls <miguel.catalan.banuls@gmail.com>
2013-11-24 13:31:00 +0100

    - First commit.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+
+	void FixedUpdate (){
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal,0.0f, moveVertical);
+		rigidbody.AddForce (movement * speed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter(Collider other){
+		if(other.gameObject.tag == "Mob"){
+			other.gameObject.SetActive(false);
+		}
+	}
+
+}
