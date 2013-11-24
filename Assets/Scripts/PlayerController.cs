de2404d8fc076bb6afa4b379780dbbfd9d621ffc
Miguel Catalan Bañuls <miguel.catalan.banuls@gmail.com>
2013-11-24 14:26:38 +0100

    - Added feedback

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,14 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
 	public float speed;
+	public GUIText winText;
+	private int count;
+	public GUIText countText;
+
+	void Start(){
+		count = 0;
+		SetCountText();
+	}
 
 	void FixedUpdate (){
 		float moveHorizontal = Input.GetAxis("Horizontal");
@@ -16,6 +24,16 @@ public class PlayerController : MonoBehaviour {
 	void OnTriggerEnter(Collider other){
 		if(other.gameObject.tag == "Mob"){
 			other.gameObject.SetActive(false);
+			count++;
+			SetCountText();
+		}
+	}
+
+	void SetCountText(){
+		countText.text = "Count: " + count.ToString ();
+		if(count >= 8){
+			winText.gameObject.SetActive(true);
+			winText.text = "You win this time!¬¬'";
 		}
 	}
 
